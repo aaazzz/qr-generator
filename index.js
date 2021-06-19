8e3ead5fef700466f06ca00e993a7f9c32ffc888
Matsui Akira <a.matsui0725@gmail.com>
2021-06-19 14:56:34 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 // default options
 app.use(fileUpload());
+// static files
+app.use(express.static('files'));
 
 app.post('/upload', function(req, res) {
   let sampleFile;
@@ -15,16 +18,19 @@ app.post('/upload', function(req, res) {
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/files/' + sampleFile.name;
+  const randString = uuidv4();
+  uploadPath = __dirname + '/files/' + randString;
 
   // Use the mv() method to place the file somewhere on your server
   sampleFile.mv(uploadPath, function(err) {
     if (err)
       return res.status(500).send(err);
 
-    res.send('File uploaded!');
+    res.status(200).json ({
+      file: randString
+    });
   });
 });
 
 app.listen(8888);
-
+console.log('http://localhost:8888!');
